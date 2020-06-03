5dd286e7622da11be95e096c3df7f6fcdc0400ca
zz514519 <1475430017@qq.com>
2020-06-04 02:46:58 +0800

    练习

---
+++ 2020-04-12-web-HTTP协议-Servlet-3/src/com/bit/ResponseServlet.java
@@ -0,0 +1,53 @@
+package com.bit;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/response-demo")
+public class ResponseServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //影响  HTTP  响应状态的 API
+        //观察，不同的状态，会导致浏览器做出不同的后续的动作
+        //配合 Chrome 开发工具 OR Fiddler 进行观察
+        //resp.setStatus(404);
+       //resp.sendError(404,"今天真高兴");    setStatus + 填充正文
+        //resp.sendRedirect("/");   //   setStatus(302) + setHeader("Location");
+
+        //设置 HTTP 响应头
+       // resp.setHeader("Key","Value");
+       // resp.setHeader("Content-Type","zhangzhe");
+        //resp.setHeader("Content-Type","text/html;charset=utf-8");
+        //resp.setContentType("text.html;charset=utf-8"); //完全等价于 setHeader("Content-Type","text/html;charset=utf-8")
+        //resp.setContentType("text/html");
+       //resp.setCharacterEncoding("utf-8");
+
+        //填充正文内容
+        //文本 -- 主要
+        /*
+        resp.setContentType("text/plain");
+        resp.setCharacterEncoding("utf-8");
+        PrintWriter writer = resp.getWriter();
+        writer.println("你好世界");
+        writer.println("中国真美丽");
+        writer.println("大家一定强");
+        */
+
+        //不是文本 -- 图片文件
+        /*
+        resp.setContentType("text/plain");
+        resp.setCharacterEncoding("utf-8");
+        ServletOutputStream outputStream = resp.getOutputStream();
+        //写了一个你字
+        outputStream.write(0xE4);
+        outputStream.write(0xBD);
+        outputStream.write(0xA0);
+        */
+    }
+}
